e41fbac96cfb6d7d2da98b78737bd561f176982a
Matouš Michalík <encero@users.noreply.github.com>
2022-03-25 23:42:15 +0100

    Announce docker image names in push script (#7)

--- hack/push-containers.sh
+++ hack/push-containers.sh
@@ -6,10 +6,15 @@ cd ${__dir}/..
 
 source ./hack/_functions.sh
 
-echo "Pushing images to gcr.io"
+echo "Pushing images to ghcr.io"
 
-docker push "${DOCKER_BASE}reciper-api:${git_revision}"
+image="${DOCKER_BASE}reciper-api:${git_revision}"
+echo ">> pushing ${image}"
+docker push "${image}" 
 
 for tag in $(git tag --points-at=HEAD); do
-    docker push "${DOCKER_BASE}reciper-api:${tag}"
+    image="${DOCKER_BASE}reciper-api:${tag}"
+    echo ">> pushing ${image}"
+
+    docker push "${image}"
 done

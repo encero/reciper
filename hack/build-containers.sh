5d3200ed7745a2c6157fb02afdf0172a7d0e9751
Matouš Michalík <i@matousmichalik.com>
2022-03-30 00:05:09 +0200

    docker compose for local FE development

--- hack/build-containers.sh
+++ hack/build-containers.sh
@@ -6,8 +6,9 @@ cd ${__dir}/..
 
 source ./hack/_functions.sh
 
-echo "Building reciper-api"
+echo "Building reciper container"
 
-docker build $(tags_for_docker) --file docker/api.Dockerfile --build-arg build_target="cmd/api/api.go" .
+docker build --tag reciper-api:latest --file docker/go.Dockerfile --build-arg build_target="cmd/api/api.go" .
+docker build --tag reciper-gql:latest --file docker/go.Dockerfile --build-arg build_target="gql/server.go" .
 
 

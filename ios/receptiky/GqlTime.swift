8c17db7a4fb901be3c3e33fd831b1e152dfc9405
Matouš Michalík <i@matousmichalik.com>
2022-05-05 21:40:41 +0200

    Show last cooked time in the ios app

---
+++ ios/receptiky/GqlTime.swift
@@ -0,0 +1,30 @@
+//
+//  GqlTine.swift
+//  receptiky
+//
+//  Created by Matous Michalik on 24.04.2022.
+//
+
+import Foundation
+import Apollo
+
+public struct Time: JSONDecodable {
+    
+    let date: Date
+    
+    public init(jsonValue value: JSONValue) throws {
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        
+        let v = value as! String
+        let vv = dateFormatter.date(from: v)
+        
+        print("\(vv)")
+        
+        guard let stringValue = value as? String, let date = dateFormatter.date(from: stringValue) else {
+                throw JSONDecodingError.couldNotConvert(value: value, to: Time.self)
+            }
+
+        self.date = date
+    }
+}
